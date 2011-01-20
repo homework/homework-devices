e4cbafff8eb07e1a8b224f6f5486d46520d757c9
Kevin Glover <ktg@cs.nott.ac.uk>
2011-01-20 11:37:52 +0000

    Comment out unused code

--- src/uk/ac/nott/mrl/homework/server/data/Convert.java
+++ src/uk/ac/nott/mrl/homework/server/data/Convert.java
@@ -186,123 +186,123 @@ public class Convert
 		}	
 	}
 
-	private static final void convertLease(final File file, final File outFile)
-	{
-		try
-		{
-			outFile.createNewFile();
-			final BufferedWriter writer = new BufferedWriter(new FileWriter(outFile));
-			writer.write("Timestamp,Action,MAC Address,IP Address,Host Name");
-			writer.newLine();
-
-			final BufferedReader reader = new BufferedReader(new FileReader(file));
-			String line = null;
-			while ((line = reader.readLine()) != null)
-			{
-				final String outline = line.substring(0, 10) + " " + line.substring(11, 19) + ","
-						+ line.substring(20).replaceAll(";", ",");
-				writer.write(outline);
-				writer.newLine();
-			}
-
-			writer.flush();
-			writer.close();
-		}
-		catch (final Exception e)
-		{
-			e.printStackTrace();
-		}
-	}
-
-	private static final void convertLink(final File file, final File outFile)
-	{
-		try
-		{
-			outFile.createNewFile();
-			final BufferedWriter writer = new BufferedWriter(new FileWriter(outFile));
-			writer.write("Timestamp,MAC Address,RSSI,Retry Count,Number of Packets,Number of Bytes");
-			writer.newLine();
-
-			final BufferedReader reader = new BufferedReader(new FileReader(file));
-			String line = null;
-			while ((line = reader.readLine()) != null)
-			{
-				final String timestamp = line.substring(0, 10) + " " + line.substring(11, 19);
-				String macAddress = line.substring(20, line.indexOf(';'));
-				while(macAddress.length() < 12)
-				{
-					macAddress = "0" + macAddress;
-				}
-				
-				macAddress = macAddress.substring(0,2) + ":" + macAddress.substring(2,4) + ":" + macAddress.substring(4,6) + ":" + macAddress.substring(6,8) + ":" + macAddress.substring(8,10) + ":" + macAddress.substring(10);		
-				
-				String outline = timestamp + "," + macAddress + line.substring(line.indexOf(';')).replaceAll(";", ",");
-				writer.write(outline);
-				writer.newLine();
-			}
-
-			writer.flush();
-			writer.close();
-		}
-		catch (final Exception e)
-		{
-			e.printStackTrace();
-		}
-	}
-
-	private static final void convertSystemEvents(final File file, final File outFile)
-	{
-		try
-		{
-			outFile.createNewFile();
-			final BufferedWriter writer = new BufferedWriter(new FileWriter(outFile));
-			writer.write("Timestamp,Event");
-			writer.newLine();
-
-			final BufferedReader reader = new BufferedReader(new FileReader(file));
-			String line = null;
-			while ((line = reader.readLine()) != null)
-			{
-				final String outline = line.substring(0, 10) + " " + line.substring(11, 19) + ","
-						+ line.substring(20).replaceAll(";", ",");
-				writer.write(outline);
-				writer.newLine();
-			}
-
-			writer.flush();
-			writer.close();
-		}
-		catch (final Exception e)
-		{
-			e.printStackTrace();
-		}
-	}
-
-	private static final void convertUserEvents(final File file, final File outFile)
-	{
-		try
-		{
-			outFile.createNewFile();
-			final BufferedWriter writer = new BufferedWriter(new FileWriter(outFile));
-			writer.write("Timestamp,Application,Type,Payload");
-			writer.newLine();
-
-			final BufferedReader reader = new BufferedReader(new FileReader(file));
-			String line = null;
-			while ((line = reader.readLine()) != null)
-			{
-				final String outline = line.substring(0, 10) + " " + line.substring(11, 19) + ","
-						+ line.substring(20).replaceAll(";", ",");
-				writer.write(outline);
-				writer.newLine();
-			}
-
-			writer.flush();
-			writer.close();
-		}
-		catch (final Exception e)
-		{
-			e.printStackTrace();
-		}
-	}
+//	private static final void convertLease(final File file, final File outFile)
+//	{
+//		try
+//		{
+//			outFile.createNewFile();
+//			final BufferedWriter writer = new BufferedWriter(new FileWriter(outFile));
+//			writer.write("Timestamp,Action,MAC Address,IP Address,Host Name");
+//			writer.newLine();
+//
+//			final BufferedReader reader = new BufferedReader(new FileReader(file));
+//			String line = null;
+//			while ((line = reader.readLine()) != null)
+//			{
+//				final String outline = line.substring(0, 10) + " " + line.substring(11, 19) + ","
+//						+ line.substring(20).replaceAll(";", ",");
+//				writer.write(outline);
+//				writer.newLine();
+//			}
+//
+//			writer.flush();
+//			writer.close();
+//		}
+//		catch (final Exception e)
+//		{
+//			e.printStackTrace();
+//		}
+//	}
+//
+//	private static final void convertLink(final File file, final File outFile)
+//	{
+//		try
+//		{
+//			outFile.createNewFile();
+//			final BufferedWriter writer = new BufferedWriter(new FileWriter(outFile));
+//			writer.write("Timestamp,MAC Address,RSSI,Retry Count,Number of Packets,Number of Bytes");
+//			writer.newLine();
+//
+//			final BufferedReader reader = new BufferedReader(new FileReader(file));
+//			String line = null;
+//			while ((line = reader.readLine()) != null)
+//			{
+//				final String timestamp = line.substring(0, 10) + " " + line.substring(11, 19);
+//				String macAddress = line.substring(20, line.indexOf(';'));
+//				while(macAddress.length() < 12)
+//				{
+//					macAddress = "0" + macAddress;
+//				}
+//				
+//				macAddress = macAddress.substring(0,2) + ":" + macAddress.substring(2,4) + ":" + macAddress.substring(4,6) + ":" + macAddress.substring(6,8) + ":" + macAddress.substring(8,10) + ":" + macAddress.substring(10);		
+//				
+//				String outline = timestamp + "," + macAddress + line.substring(line.indexOf(';')).replaceAll(";", ",");
+//				writer.write(outline);
+//				writer.newLine();
+//			}
+//
+//			writer.flush();
+//			writer.close();
+//		}
+//		catch (final Exception e)
+//		{
+//			e.printStackTrace();
+//		}
+//	}
+//
+//	private static final void convertSystemEvents(final File file, final File outFile)
+//	{
+//		try
+//		{
+//			outFile.createNewFile();
+//			final BufferedWriter writer = new BufferedWriter(new FileWriter(outFile));
+//			writer.write("Timestamp,Event");
+//			writer.newLine();
+//
+//			final BufferedReader reader = new BufferedReader(new FileReader(file));
+//			String line = null;
+//			while ((line = reader.readLine()) != null)
+//			{
+//				final String outline = line.substring(0, 10) + " " + line.substring(11, 19) + ","
+//						+ line.substring(20).replaceAll(";", ",");
+//				writer.write(outline);
+//				writer.newLine();
+//			}
+//
+//			writer.flush();
+//			writer.close();
+//		}
+//		catch (final Exception e)
+//		{
+//			e.printStackTrace();
+//		}
+//	}
+//
+//	private static final void convertUserEvents(final File file, final File outFile)
+//	{
+//		try
+//		{
+//			outFile.createNewFile();
+//			final BufferedWriter writer = new BufferedWriter(new FileWriter(outFile));
+//			writer.write("Timestamp,Application,Type,Payload");
+//			writer.newLine();
+//
+//			final BufferedReader reader = new BufferedReader(new FileReader(file));
+//			String line = null;
+//			while ((line = reader.readLine()) != null)
+//			{
+//				final String outline = line.substring(0, 10) + " " + line.substring(11, 19) + ","
+//						+ line.substring(20).replaceAll(";", ",");
+//				writer.write(outline);
+//				writer.newLine();
+//			}
+//
+//			writer.flush();
+//			writer.close();
+//		}
+//		catch (final Exception e)
+//		{
+//			e.printStackTrace();
+//		}
+//	}
 }
