21674da25a6d10f3eb1e4d69d01f9b96759c1a6b
Kevin Glover <ktg@cs.nott.ac.uk>
2011-10-11 11:34:50 +0100

    Change structure of comic catalogue page to match original

--- src/uk/ac/nott/mrl/homework/server/model/Catalogue.java
+++ src/uk/ac/nott/mrl/homework/server/model/Catalogue.java
@@ -9,36 +9,48 @@ import com.google.gson.annotations.Expose;
 
 public class Catalogue
 {
-	@Expose
-	private Map<String,List<String>> subjects = new HashMap<String, List<String>>();
-	@Expose	
-	private Map<String,String> bundlelookup = new HashMap<String, String>();
-	@Expose	
-	private Map<String, Map<String, String>> devices = new HashMap<String, Map<String,String>>();
+	private class InnerCatalogue
+	{
+		@Expose
+		private Dynamic dynamic = new Dynamic(); 
+	}
 	
+	private class Dynamic
+	{
+		@Expose
+		private Map<String,List<String>> subjects = new HashMap<String, List<String>>();
+		@Expose	
+		private Map<String,String> bundlelookup = new HashMap<String, String>();
+		@Expose	
+		private Map<String, Map<String, String>> devices = new HashMap<String, Map<String,String>>();		
+	}
+	
+	@Expose
+	private InnerCatalogue catalogue = new InnerCatalogue();
+		
 	public void addSubject(String subject, String ip)
 	{
-		List<String> ips = subjects.get(subject);
+		List<String> ips = catalogue.dynamic.subjects.get(subject);
 		if(ips == null)
 		{
 			ips = new ArrayList<String>();
-			subjects.put(subject, ips);
+			catalogue.dynamic.subjects.put(subject, ips);
 		}
 		ips.add(ip);
 	}
 	
 	public void addBundle(String id, String bundle)
 	{
-		bundlelookup.put(id, bundle);
+		catalogue.dynamic.bundlelookup.put(id, bundle);
 	}
 	
 	public void addName(String ip, String name)
 	{
-		Map<String, String> metadata = devices.get(ip);
+		Map<String, String> metadata = catalogue.dynamic.devices.get(ip);
 		if(metadata == null)
 		{
 			metadata = new HashMap<String, String>();
-			devices.put(ip, metadata);
+			catalogue.dynamic.devices.put(ip, metadata);
 		}
 		metadata.put("name", name);
 	}
