7b31fcf469b3b421a3d03ee7ed32c3453c0419c5
Kevin Glover <ktg@cs.nott.ac.uk>
2011-10-11 10:38:44 +0100

    New metadata page for comic client

--- src/uk/ac/nott/mrl/homework/server/model/NoxStatus.java
+++ src/uk/ac/nott/mrl/homework/server/model/NoxStatus.java
@@ -9,17 +9,15 @@ public class NoxStatus
 
 	public static void parseResultSet(final String results, final Model model)
 	{
-		final String[] lines = results.split("\n");
-		if (!lines[0].endsWith("<|>0<|>0<|>"))
+		final ResultSet result = new ResultSet(results);
+		if(result.getSize() != 0)
 		{
-			System.out.println("Statuses: " + results);
+			System.out.println("New statuses: " + result.getSize());
 		}
-
-		for (int index = 2; index < lines.length; index++)
-		{
+		for(String[] columns: result)
+		{			
 			try
 			{
-				final String[] columns = lines[index].split("<\\|>");
 				final NoxStatus status = new NoxStatus();
 				final String time = columns[0].substring(1, columns[0].length() - 1);
 				final long timeLong = Long.parseLong(time, 16);
