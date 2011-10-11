37c9f877f74388e9a8887b7e6e9bc778b21b5114
Kevin Glover <ktg@cs.nott.ac.uk>
2011-10-11 11:42:04 +0100

    Fix null pointer exception

--- src/uk/ac/nott/mrl/homework/server/model/Metadata.java
+++ src/uk/ac/nott/mrl/homework/server/model/Metadata.java
@@ -3,9 +3,13 @@ package uk.ac.nott.mrl.homework.server.model;
 import java.util.Collection;
 import java.util.Map;
 
+import com.google.gson.annotations.Expose;
+
 public class Metadata
 {
+	@Expose
 	private Map<String, String> owners;
+	@Expose
 	private Collection<String> types;
 	
 	public Iterable<String> getTypes()
